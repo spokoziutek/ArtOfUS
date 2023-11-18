d9aa8b34a6bd5c8ff6d9bd35c358005d5316911c
spokoziutek <ubieskiewicz@gmail.com>
2023-11-18 16:57:17 +0100

    Development of the site

---
+++ scripts/smoothscrolling.js
@@ -0,0 +1,11 @@
+$(document).ready(function() {
+    $('nav a').on('click', function(event) {
+      event.preventDefault();
+      
+      const target = $(this).attr('href');
+      
+      $('html, body').animate({
+        scrollTop: $(target).offset().top
+      }, 1000);
+    });
+  });
