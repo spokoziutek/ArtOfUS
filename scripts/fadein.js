041cf3e5533b3e32f7dadb48bbe8d74163f158de
spokoziutek <ubieskiewicz@gmail.com>
2023-11-25 01:48:46 +0100

    Little tweaks

--- scripts/fadein.js
+++ scripts/fadein.js
@@ -1,4 +1,13 @@
 $(document).ready(function(){
 
-    $('body').addClass('fade-in-slide');
+    $('h1').addClass('fade-in-slide');
+    $('h2').addClass('fade-in-slide');
+    $('h3').addClass('fade-in-slide');
+    $('img').addClass('fade-in-slide');
+    $('i').addClass('fade-in-slide');
+    $('form').addClass('fade-in-slide');
+    $('.navbar-brand').addClass('fade-in-slide');
+    $('.nav-item').addClass('fade-in-slide');
+    $('p').addClass('fade-in-slide');
+    
 });
